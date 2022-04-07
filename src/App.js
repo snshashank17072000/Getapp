fb6bce19d442a732a4c823bbdfd54edf8c288489
Shashank <snshashank17072000@gmail.com>
2022-04-07 14:17:39 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+function App()
+ {
+    return (
+      <div className="App">
+            <header className="App-header">
+        <div> x</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
